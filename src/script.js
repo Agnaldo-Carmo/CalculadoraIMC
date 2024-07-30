4c91ed8386192dc198a10f0d12e8a2fb755b5943
Agnaldo-Carmo <agnaldocarmo10@gmail.com>
2024-07-30 13:12:58 -0300

    Encapsulamento das funçoes "calcularIMC", "getClassifocação" e "limpar"

--- src/script.js
+++ src/script.js
@@ -1,60 +1,33 @@
+//Calcula o IMC e atribui em uma variavel
+function calcularIMC(peso, altura) {
+    let imc = peso / (altura * altura)
 
+    return imc;
+}
 
-window.onload = function () {
+/*Determina a Classificação do IMC obtido comparando o Resultado com os parametros estabelecidos 
+e guarda o valor em uma variavel*/
+function getClassificacao(imc) {
+    let classificacao;
+    
+    if (imc < 18.5) {
+        classificacao = "abaixo do peso"
 
-    //Recupera o elemento botão pelo id "calcular"
-    document.getElementById("Calcular").addEventListener("click", function () {
+    } else if (18.5 <= imc && imc < 24.9) {
+        classificacao = "no peso normal"
 
-        //Atribui os Valores dos formularios em suas respectivas variaveis
-        altura = document.getElementById("altura").value;
-        peso = document.getElementById("peso").value;
+    } else if (25 <= imc && imc < 29.9) {
+        classificacao = "com sobrepeso"
 
-        //Calcula o IMC e atribui em uma variavel
-        imc = peso / (altura * altura);
+    } else if (imc >= 30) {
+        classificacao = "com obesidade"
+    }
 
-        /*Determina a Classificação do IMC obtido comparando o Resultado com os parametros estabelecidos 
-        e guarda o valor em uma variavel*/
+    return classificacao;
+}
 
-        if (imc < 18.5) {
-
-            classificacao = "abaixo do peso"
-
-        } else if (18.5 <= imc && imc < 24.9) {
-
-            classificacao = "no peso normal"
-
-        } else if (25 <= imc && imc < 29.9) {
-
-            classificacao = "com sobrepeso"
-
-        } else if (imc >= 30) {
-
-            classificacao = "com obesidade"
-
-        }
-
-        //Faz uma validação para impedir que os campos altura e peso sejam deixados vazios
-        if (altura == "" || peso == "") {
-
-            alert("Favor preencher todos os dados")
-
-        } else {
-
-            //Atribui o valor calculado do IMC ao campo resultado arrendondando para duas casas decimais
-
-            document.getElementById("resultado").value = imc.toFixed(2);
-
-            /*Exibe para o usuario uma Mensagem concatenada com o valor da variavel "classificacao" 
-             informando sua situação com base no IMC*/
-
-            document.getElementById("classificacao").value = "Você está " + classificacao;
-
-
-        }
-
-    })
-
-    //Limpa todos os campos permitindo que o usuario digite novamente ou faça ou novo calculo
+//Limpa todos os campos permitindo que o usuario digite novamente ou faça ou novo calculo
+function limpar() {
     document.getElementById("Limpar").addEventListener("click", function () {
 
         altura = document.getElementById("altura").value = "";
@@ -63,6 +36,40 @@ window.onload = function () {
         document.getElementById("classificacao").value = "";
 
     })
+}
+
+window.onload = function () {
+
+    //Recupera o elemento botão pelo id "calcular"
+    document.getElementById("Calcular").addEventListener("click", function () {
+
+        //Atribui os Valores dos formularios em suas respectivas variaveis
+        let altura = document.getElementById("altura").value;
+        let peso = document.getElementById("peso").value;
+
+        //Chama a função para calcular o IMC
+        let imc = calcularIMC(peso, altura);
+
+        //Chama a função para obter a classificação
+        let classificacao = getClassificacao(imc);
+
+        //Faz uma validação para impedir que os campos altura e peso sejam deixados vazios
+        if (altura == "" || peso == "") {
+
+            alert("Favor preencher todos os dados");
+
+        } else {
+            //Atribui o valor calculado do IMC ao campo resultado arrendondando para duas casas decimais
+            document.getElementById("resultado").value = imc.toFixed(2);
+
+            /*Exibe para o usuario uma Mensagem concatenada com o valor da variavel "classificacao" 
+             informando sua situação com base no IMC*/
+            document.getElementById("classificacao").value = "Você está " + classificacao;
+        }
+    })
+
+    //Chama a função para limpar os formularios
+    limpar();
 }
 
 /*Exemplos de Teste:
@@ -86,6 +93,5 @@ Altura = 1,65
 Peso = 95 
 Resultado = 34,89
 Classificação = obesidade.
-
 */
 
